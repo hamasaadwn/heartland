d62c7f5ea141226e22b30a954874ff6331df0439
hama saadwn <hamasaadwn@gmail.com>
2021-12-16 11:43:44 +0300

    database added and user model created

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
