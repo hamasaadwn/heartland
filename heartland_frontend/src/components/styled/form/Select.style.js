b7588246ce72073d872d0036882a29f033d8264e
hama saadwn <hamasaadwn@gmail.com>
2022-01-04 09:38:15 +0300

    added add conact info form

---
+++ heartland_frontend/src/components/styled/form/Select.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Select = styled.select`
+  padding: 5px;
+  font-size: 25px;
+  width: 100%;
+  /* border: none; */
+  border-radius: 5px;
+`;
--- heartland_frontend/src/screens/admin/contact/Contact.js
+++ heartland_frontend/src/screens/admin/contact/Contact.js
@@ -7,11 +7,21 @@ import {
   changeNavbar,
 } from "../../../actions/rootActions";
 import { AdminContainer } from "../../../components/styled/AdminContainer";
+import { Button } from "../../../components/styled/form/Button.style";
+import { Input } from "../../../components/styled/form/Input.style";
+import { Select } from "../../../components/styled/form/Select.style";
+import { Spacer } from "../../../components/styled/Spacer.style";
+import { Table } from "../../../components/styled/Table.style";
 import { TwoColFlex } from "../../../components/styled/TwoColFlex.style";
 
 const Contact = () => {
   const dispatch = useDispatch();
   const { contacts } = useSelector((state) => state.contacts);
+
+  const [formData, setFormData] = useState({
+    type: "phone",
+    value: "",
+  });
 
   useEffect(() => {
     dispatch(changeNavbar("side"));
@@ -19,20 +29,74 @@ const Contact = () => {
     dispatch(loadContact());
   }, []);
 
+  const setInput = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <AdminContainer>
-      <ul>
-        {
-          console.log(contacts) /* {contacts &&
-          contacts.map((c) => (
-            <li>
-              <TwoColFlex className="space_between">
-                <div>test</div>
-              </TwoColFlex>
-            </li>
-          ))} */
-        }
-      </ul>
+      <form>
+        <TwoColFlex>
+          <div>
+            <label>Type</label>
+            <Select
+              name="type"
+              value={formData.type}
+              onChange={setInput}
+              require={true}
+            >
+              <option value="phone">Phone</option>
+              <option value="email">Email</option>
+              <option value="facebook">Facebook</option>
+              <option value="twitter">Twitter</option>
+              <option value="instagram">Instagram</option>
+              <option value="youtube">Youtube</option>
+              <option value="linkedin">Linkedin</option>
+            </Select>
+          </div>
+          <div>
+            <label>Content</label>
+            <Input
+              bg="#ffffffdb"
+              fg="#3d3d3d80"
+              placeholder="Content"
+              type="text"
+              name="value"
+              value={formData.value}
+              onChange={setInput}
+              required={true}
+            />
+          </div>
+        </TwoColFlex>
+        <Button bg="#02a89e" fg="#ffffff">
+          Add Contact Info
+        </Button>
+      </form>
+      <Spacer top="40px" />
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Content</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts &&
+            contacts.map((c) => (
+              <tr key={c._id}>
+                <td>{c.type}</td>
+                <td>{c.value}</td>
+                <td>Actions</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+      <ul></ul>
     </AdminContainer>
   );
 };
