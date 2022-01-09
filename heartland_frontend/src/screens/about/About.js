fb689d49a7b66cee8c4b8f038872966cfc7a71c1
hama saadwn <hamasaadwn@gmail.com>
2022-01-09 11:48:40 +0300

    about page updatge

--- heartland_frontend/src/screens/about/About.js
+++ heartland_frontend/src/screens/about/About.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 import { Container } from "../../components/styled/Container.style";
 import {
@@ -8,46 +9,38 @@ import {
 } from "../../actions/rootActions";
 
 import { AboutContainer } from "./About.Styles";
+import { loadContent } from "../../actions/contentActions";
 
 const About = () => {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const { content } = useSelector((state) => state.content);
+  const { language } = useSelector((state) => state.root);
 
   useEffect(() => {
     dispatch(changeBackgroundToWhite());
     dispatch(changeNavbar("white"));
+    dispatch(loadContent("About"));
   }, []);
 
   return (
     <Container>
       <AboutContainer>
         <div className="about">
-          <h1>About Us</h1>
+          <h1>{t("about_us")}</h1>
         </div>
         <div className="img"></div>
         <div className="paragraph">
-          <p>
-            Officia pariatur sunt consequat enim quis. Deserunt ex et ullamco
-            excepteur consequat reprehenderit. Aute minim et excepteur eiusmod
-            sunt qui veniam elit qui pariatur tempor. Culpa quis consequat irure
-            ad ex esse sit commodo.Consectetur sit esse sunt duis deserunt
-            eiusmod esse amet sunt nulla dolore. Proident eu tempor id elit
-            ullamco aliqua enim nisi Lorem. Deserunt anim exercitation cillum
-            consequat elit. Id cupidatat aliqua ut laboris et dolore ad nisi
-            ipsum. Officia eiusmod eu enim sit. Do cupidatat magna ea cillum
-            exercitation. Adipisicing exercitation aute consectetur eiusmod eu.
-            Excepteur excepteur dolore laborum consequat cillum. Consequat
-            reprehenderit sint consectetur cillum ea mollit nisi exercitation
-            consectetur labore. Do nulla commodo qui est tempor ex velit eu quis
-            ea. Laboris fugiat minim ad nisi cupidatat reprehenderit mollit
-            labore minim voluptate laborum aute. Laboris laboris elit deserunt
-            minim aliqua ea ullamco esse proident excepteur aliqua fugiat. Qui
-            laboris ullamco commodo veniam. Duis do dolore reprehenderit
-            cupidatat ex. Esse deserunt non in tempor dolor officia in voluptate
-            pariatur. Fugiat do nulla occaecat mollit quis est sint consequat
-            eiusmod cillum cillum laboris. Ex ex aute excepteur dolore voluptate
-            sint dolore pariatur amet est adipisicing sit sit nostrud. Non ut
-            labore nostrud irure exercitation.
-          </p>
+          {console.log(content)}
+          {content &&
+            (language === "en" ? (
+              <p>{content.contentEn}</p>
+            ) : language === "ar" ? (
+              <p>{content.contentAr}</p>
+            ) : (
+              ""
+            ))}
         </div>
       </AboutContainer>
     </Container>
