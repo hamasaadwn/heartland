926f5c62951ab8c8d3d23bf6369a94c21fc35227
hama saadwn <hamasaadwn@gmail.com>
2022-01-25 16:18:15 +0300

    removed one line of code

--- heartland_frontend/src/screens/guide/Guide.js
+++ heartland_frontend/src/screens/guide/Guide.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 import {
