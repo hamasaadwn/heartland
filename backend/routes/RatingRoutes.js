dd4779b631044d01e8998012a13ac5c01ac91a6f
zhir-aryan <zhiraryan@gmail.com>
2022-06-16 17:46:12 +0300

    visitor

--- backend/routes/RatingRoutes.js
+++ backend/routes/RatingRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 
 import { rate, result, getAllRating } from "../controllers/RatingController.js";
 
+
 const router = express.Router();
+
 
 router.route("/:scope").post(rate).get(result);
 router.route("/").get(result);
