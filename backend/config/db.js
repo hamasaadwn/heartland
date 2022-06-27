ef355c7daa39a622733f728c762fbc31eb2f3d25
root <root@_HOSTNAME_>
2022-06-27 12:06:22 +0200

    working commit on server

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://localhost:27017/heartland");
     console.log(`mongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (err) {
     console.error(`Error: ${err.message}`.red.bold);
